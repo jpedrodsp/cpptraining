6147399fde09adf0dc74af926e9c82f9b1727f00
Joao Pedro Patrocinio <jpedro9966@gmail.com>
2021-04-22 23:55:31 -0300

    Added benchmarks timing results output

--- 2021_04_22/fibbonaci/main.cpp
+++ 2021_04_22/fibbonaci/main.cpp
@@ -1,36 +1,55 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 #include "fibbonaci_recursive.h"
 #include "fibbonaci_dynamic.h"
 #include "fibbonaci_iterative.h"
 
 void benchmark_recursive(std::vector<unsigned int>& fibnums)
 {
+    std::chrono::steady_clock::time_point timer_start, timer_end;
+    int64_t diff;
     for (unsigned int num : fibnums)
     {
         std::cout << "Fibbonaci (recursive) for " << num << " ... ";
+        timer_start = std::chrono::steady_clock::now();
         const unsigned int result = FibbonaciRecursive::fibbonaci(num);
-        std::cout << result << std::endl;
+        timer_end = std::chrono::steady_clock::now();
+        diff = std::chrono::duration_cast<std::chrono::microseconds>(timer_end - timer_start).count();
+        std::cout << result;
+        std::cout << " - " << diff << "[µs]" << std::endl;
     }
 }
 void benchmark_iterative(std::vector<unsigned int>& fibnums)
 {
+    std::chrono::steady_clock::time_point timer_start, timer_end;
+    int64_t diff;
     for (unsigned int num : fibnums)
     {
         std::cout << "Fibbonaci (iterative) for " << num << " ... ";
+        timer_start = std::chrono::steady_clock::now();
         const unsigned int result = FibbonaciIterative::fibbonaci(num);
-        std::cout << result << std::endl;
+        timer_end = std::chrono::steady_clock::now();
+        diff = std::chrono::duration_cast<std::chrono::microseconds>(timer_end - timer_start).count();
+        std::cout << result;
+        std::cout << " - " << diff << "[µs]" << std::endl;
     }
 }
 void benchmark_dynamic(std::vector<unsigned int>& fibnums)
 {
+    std::chrono::steady_clock::time_point timer_start, timer_end;
+    int64_t diff;
     for (unsigned int num : fibnums)
     {
         FIBBONACI_DYNAMIC_STORAGE_TYPE _fibbonaci_dynamic_storage;
         _fibbonaci_dynamic_storage = FibbonaciDynamic::create_fibbonaci_dynamic_storage(num);
         std::cout << "Fibbonaci (dynamic) for " << num << " ... ";
+        timer_start = std::chrono::steady_clock::now();
         const unsigned int result = FibbonaciDynamic::fibbonaci(num);
-        std::cout << result << std::endl;
+        timer_end = std::chrono::steady_clock::now();
+        diff = std::chrono::duration_cast<std::chrono::microseconds>(timer_end - timer_start).count();
+        std::cout << result;
+        std::cout << " - " << diff << "[µs]" << std::endl;
     }
 }
 
