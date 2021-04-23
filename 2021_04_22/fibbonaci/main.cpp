323cf94b3f91dca6b4de0149911ed9377c5ae62b
Joao Pedro Patrocinio <jpedro9966@gmail.com>
2021-04-22 23:49:16 -0300

    Split code parts into functions

--- 2021_04_22/fibbonaci/main.cpp
+++ 2021_04_22/fibbonaci/main.cpp
@@ -4,22 +4,27 @@
 #include "fibbonaci_dynamic.h"
 #include "fibbonaci_iterative.h"
 
-int main(int argc, char** argv) {
-    std::vector<unsigned int> nums {1, 2, 3, 5, 8, 24, 30};
-
-    for (unsigned int num : nums)
+void benchmark_recursive(std::vector<unsigned int>& fibnums)
+{
+    for (unsigned int num : fibnums)
     {
         std::cout << "Fibbonaci (recursive) for " << num << " ... ";
         const unsigned int result = FibbonaciRecursive::fibbonaci(num);
         std::cout << result << std::endl;
     }
-    for (unsigned int num : nums)
+}
+void benchmark_iterative(std::vector<unsigned int>& fibnums)
+{
+    for (unsigned int num : fibnums)
     {
         std::cout << "Fibbonaci (iterative) for " << num << " ... ";
         const unsigned int result = FibbonaciIterative::fibbonaci(num);
         std::cout << result << std::endl;
     }
-    for (unsigned int num : nums)
+}
+void benchmark_dynamic(std::vector<unsigned int>& fibnums)
+{
+    for (unsigned int num : fibnums)
     {
         FIBBONACI_DYNAMIC_STORAGE_TYPE _fibbonaci_dynamic_storage;
         _fibbonaci_dynamic_storage = FibbonaciDynamic::create_fibbonaci_dynamic_storage(num);
@@ -27,5 +32,12 @@ int main(int argc, char** argv) {
         const unsigned int result = FibbonaciDynamic::fibbonaci(num);
         std::cout << result << std::endl;
     }
+}
+
+int main(int argc, char** argv) {
+    std::vector<unsigned int> nums {1, 2, 3, 5, 8, 24, 30};
+    benchmark_recursive(nums);
+    benchmark_iterative(nums);
+    benchmark_dynamic(nums);
     return 0;
 }
